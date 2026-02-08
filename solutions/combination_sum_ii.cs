8b9d1cb4091c2a02e59d0092860969079a422767
Vikas Gautam <vikasgautamug2003@gmail.com>
2026-02-08 17:13:35 +0530

    Update solution for combination_sum_ii

--- solutions/combination_sum_ii.cs
+++ solutions/combination_sum_ii.cs
@@ -1,4 +1,16 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int size = candidates.length;
+        List<List<Integer>> arr = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        helper(candidates, target, arr, res, 0, 0);
+    }
+
+    void helper(int[] candidates, int target, List<List<Integer>> arr,  List<Integer> res, int index, int sum)
+…
         res.remove(size() - 1);
+
+        helper(candidates, target, arr, res, index + 1, sum);
 
     }
 }
