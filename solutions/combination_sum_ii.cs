b93c79b605b96c6a8cb43fff1094b781bb9c6533
Vikas Gautam <vikasgautamug2003@gmail.com>
2026-02-08 17:16:33 +0530

    Update solution for combination_sum_ii

--- solutions/combination_sum_ii.cs
+++ solutions/combination_sum_ii.cs
@@ -1,5 +1,9 @@
-        res.remove(res.size() - 1);
-    }
-}
+        if (i > index && candidates[i] == candidates[i - 1]) continue;
 
-}
+        if (candidates[i] > target) break;
+
+        res.add(candidates[i]);
+
+        helper(candidates, target - candidates[i], arr, res, i + 1);
+
+        res.remove(res.size() - 1);
