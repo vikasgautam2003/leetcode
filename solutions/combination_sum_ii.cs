c502f51f3b9a38471add5005488adf79c994ae44
Vikas Gautam <vikasgautamug2003@gmail.com>
2026-02-08 17:16:15 +0530

    Update solution for combination_sum_ii

--- solutions/combination_sum_ii.cs
+++ solutions/combination_sum_ii.cs
@@ -1,7 +1,5 @@
-        int ans = candidates[index];
-        res.add(ans);
-        helper(candidates, target, arr, res, index + 1, sum + ans);
-
         res.remove(res.size() - 1);
+    }
+}
 
-        helper(candidates, target, arr, res, index + 1, sum);
+}
